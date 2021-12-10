97ea9a7a9f3607bab28617a461166caf4dd7e822
Domonkos Papp <pappdomonkos96@gmail.com>
2021-12-10 18:53:29 +0100

    Setup eslint

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,9 +1,15 @@
 export default {
-    routes: [
-      {
-        match: 'routes',
-        src: '.*',
-        dest: '/index.html',
-      },
-    ],
+  routes: [
+    {
+      match: 'routes',
+      src: '.*',
+      dest: '/index.html',
+    },
+  ],
+  plugins: [
+    ['@canarise/snowpack-eslint-plugin', {
+      globs: ['src/**/*.js', 'src/**/*.js'],
+      formatter: 'stylish',
+    }],
+  ],
 };
