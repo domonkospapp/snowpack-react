8136c7e5593bb6038a9b6eec5ee7b15e73405c74
Domonkos Papp <pappdomonkos96@gmail.com>
2021-12-10 19:32:21 +0100

    List books on the shop page

--- src/routes/ShopPage.js
+++ src/routes/ShopPage.js
@@ -1,5 +1,28 @@
 import { html } from 'htm/preact';
+import { useState, useEffect } from 'preact/hooks';
+import { collection, onSnapshot } from 'firebase/firestore';
+import db from '../utils/firebase';
 
-const ShopPage = () => html`<h2>List of books</h2>`;
+const ShopPage = () => {
+  const [books, setBooks] = useState([]);
+  const collectionRef = collection(db, 'books');
+
+  const updateBooks = (snapshot) => {
+    setBooks(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+  };
+
+  useEffect(() => {
+    onSnapshot(collectionRef, updateBooks);
+  }, []);
+
+  const renderBook = (book) => (html`
+    <p>${book.title} ${book.author}</p>`
+  );
+
+  return (html`
+    <h2>List of books</h2>
+    ${books.map(renderBook)}
+  `);
+};
 
 export default ShopPage;
